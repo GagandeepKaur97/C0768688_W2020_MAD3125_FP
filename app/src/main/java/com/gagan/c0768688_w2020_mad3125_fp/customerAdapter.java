d772236a5747c7384e7df4c579c18f05f2b9d6a5
gagandeep kaur <sherry2312.gk@gmail.com>
2020-04-15 16:48:25 -0400

    [gk] recyclerview adaptercode

--- app/src/main/java/com/gagan/c0768688_w2020_mad3125_fp/customerAdapter.java
+++ app/src/main/java/com/gagan/c0768688_w2020_mad3125_fp/customerAdapter.java
@@ -1,4 +1,44 @@
 package com.gagan.c0768688_w2020_mad3125_fp;
 
-public class customerAdapter  {
-}
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class customerAdapter extends RecyclerView.Adapter<customerAdapter.customerviewholder> {
+    private ArrayList<customer> customersArrayList;
+
+    public customerAdapter(ArrayList<customer> customersArrayList) {
+        this.customersArrayList = customersArrayList;
+    }
+
+    @NonNull
+    @Override
+    public customerAdapter.customerviewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull customerAdapter.customerviewholder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+
+    }
+
+ public class customerviewholder extends RecyclerView.ViewHolder {
+     TextView txtCustomerName;
+
+     public customerviewholder(@NonNull View itemView) {
+         super(itemView);
+
+         txtCustomerName = itemView.findViewById(R.id.image_name);
+     }
+ }}
--- app/src/main/java/com/gagan/c0768688_w2020_mad3125_fp/list_of_customers.java
+++ app/src/main/java/com/gagan/c0768688_w2020_mad3125_fp/list_of_customers.java
@@ -1,15 +1,66 @@
 package com.gagan.c0768688_w2020_mad3125_fp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
 
 public class list_of_customers extends AppCompatActivity {
-
+    private RecyclerView rvCustomerslist;
+    private ArrayList<customer> customers;
+    private customerAdapter customersAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_of_customers);
+        rvCustomerslist = findViewById(R.id.rvcustomerlist);
+
+        customersInfo();
+
+        customersAdapter = new customerAdapter(customers);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+
+        rvCustomerslist.setLayoutManager(mLayoutManager);
+
+        rvCustomerslist.setAdapter(customersAdapter);
+    }
+
+    private void customersInfo(){
+        customers = new ArrayList<>();
+        customers.add(new customer("C0001","Gagandeep","kaur","gagan@gmail.com"));
+        customers.add(new customer("C0002","Evneet","kaur","nancy@gmail.com"));
+        customers.add(new customer("C0003","Kamalpreet","Kaur","kamal@gmail.com"));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {         //https://javatpoint.com/android-option-menu-example
+
+        getMenuInflater().inflate(R.menu.newcustomer, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu1:
+                Intent intent2 = new Intent(list_of_customers.this, details_customers.class);
+                startActivity(intent2);
+
+                return true;
+            case R.id.menu2:
+
+                Intent intent = new Intent(list_of_customers.this, login_screen.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
