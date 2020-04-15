566b01de96f4b84b8afc61d2a0bbe6561f613e24
gagandeep kaur <sherry2312.gk@gmail.com>
2020-04-15 14:49:13 -0400

    [gk] variables inilized

--- app/src/main/java/com/gagan/c0768688_w2020_mad3125_fp/newHydroBill.java
+++ app/src/main/java/com/gagan/c0768688_w2020_mad3125_fp/newHydroBill.java
@@ -3,12 +3,26 @@ package com.gagan.c0768688_w2020_mad3125_fp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class newHydroBill extends AppCompatActivity {
+    private TextInputEditText HBillID;
+    private TextInputEditText HBillDate;
+    private TextInputEditText Agencyname;
+    private TextInputEditText Unitsconsumed;
+    private Button register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_hydro_bill);
+        HBillID = findViewById(R.id.hydroBillIDt);
+        HBillDate = findViewById(R.id.hydroBillDatet);
+        Agencyname = findViewById(R.id.hydroAgencyNamet);
+        Unitsconsumed = findViewById(R.id.hydroUnitConsumedt);
+        register = findViewById(R.id.hydrobutton);
+
     }
 }
