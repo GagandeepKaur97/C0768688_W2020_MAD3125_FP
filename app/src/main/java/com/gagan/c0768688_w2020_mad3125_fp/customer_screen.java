04d18ad6ad7850e7e44eaff87a92bd465aa391ba
gagandeep kaur <sherry2312.gk@gmail.com>
2020-04-14 20:45:50 -0400

    [gk]added menu

--- app/src/main/java/com/gagan/c0768688_w2020_mad3125_fp/customer_screen.java
+++ app/src/main/java/com/gagan/c0768688_w2020_mad3125_fp/customer_screen.java
@@ -1,14 +1,25 @@
 package com.gagan.c0768688_w2020_mad3125_fp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class customer_screen extends AppCompatActivity {
-
+    private RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_screen);
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+        recyclerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(customer_screen.this,customer_details.class);
+                startActivity(intent);
+            }
+        });
     }
 }
