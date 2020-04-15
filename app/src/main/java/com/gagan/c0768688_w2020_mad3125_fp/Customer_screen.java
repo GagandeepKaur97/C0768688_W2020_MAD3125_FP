2e69d8d0b1723e79b1e38d717f553bdcddf340fc
gagandeep kaur <sherry2312.gk@gmail.com>
2020-04-15 15:59:16 -0400

    [gk] customer screen

--- app/src/main/java/com/gagan/c0768688_w2020_mad3125_fp/Customer_screen.java
+++ app/src/main/java/com/gagan/c0768688_w2020_mad3125_fp/Customer_screen.java
@@ -3,8 +3,13 @@ package com.gagan.c0768688_w2020_mad3125_fp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Customer_screen extends AppCompatActivity {
+    private TextView CustomerId;
+    private TextView FullName;
+    private TextView Email;
+    private TextView TotalAmountToPay;
 
 
     @Override
---
+++ app/src/main/java/com/gagan/c0768688_w2020_mad3125_fp/list_of_customers.java
@@ -0,0 +1,15 @@
+package com.gagan.c0768688_w2020_mad3125_fp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class list_of_customers extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_of_customers);
+    }
+}
