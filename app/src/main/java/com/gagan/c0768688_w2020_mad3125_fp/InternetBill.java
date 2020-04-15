9f9aee9d84de7054945321bee872741c23907879
gagandeep kaur <sherry2312.gk@gmail.com>
2020-04-15 14:27:28 -0400

    [gk] constructer generated for internetbill

--- app/src/main/java/com/gagan/c0768688_w2020_mad3125_fp/InternetBill.java
+++ app/src/main/java/com/gagan/c0768688_w2020_mad3125_fp/InternetBill.java
@@ -1,4 +1,28 @@
 package com.gagan.c0768688_w2020_mad3125_fp;
 
-public class InternetBill {
+public class InternetBill extends Bill {
+    private String providername;
+    private int internetGBused;
+
+    public String getProvidername() {
+        return providername;
+    }
+
+    public void setProvidername(String providername) {
+        this.providername = providername;
+    }
+
+    public int getInternetGBused() {
+        return internetGBused;
+    }
+
+    public void setInternetGBused(int internetGBused) {
+        this.internetGBused = internetGBused;
+    }
+
+    public InternetBill(String billId, String billDate, String billType, Double totalBillAmount, String providername, int internetGBused) {
+        super(billId, billDate, billType, totalBillAmount);
+        this.providername = providername;
+        this.internetGBused = internetGBused;
+    }
 }
