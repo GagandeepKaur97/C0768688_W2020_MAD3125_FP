6b5e7a5da695c67abea31d3084ad3ee2aec98e98
gagandeep kaur <sherry2312.gk@gmail.com>
2020-04-15 14:24:31 -0400

    [gk] constructer and getter,setter generated for hydrobill

--- app/src/main/java/com/gagan/c0768688_w2020_mad3125_fp/HydroBill.java
+++ app/src/main/java/com/gagan/c0768688_w2020_mad3125_fp/HydroBill.java
@@ -1,4 +1,32 @@
 package com.gagan.c0768688_w2020_mad3125_fp;
 
-public class HydroBill {
-}
+public class HydroBill extends Bill {
+    private String agencyname;
+    private int unitsconsumed;
+
+    public String getAgencyname() {
+        return agencyname;
+    }
+
+    public void setAgencyname(String agencyname) {
+        this.agencyname = agencyname;
+    }
+
+    public int getUnitsconsumed() {
+        return unitsconsumed;
+    }
+
+    public void setUnitsconsumed(int unitsconsumed) {
+        this.unitsconsumed = unitsconsumed;
+    }
+
+    public HydroBill(String billId, String billDate, String billType, Double totalBillAmount, String agencyname, int unitsconsumed) {
+        super(billId, billDate, billType, totalBillAmount);
+        this.agencyname = agencyname;
+        this.unitsconsumed = unitsconsumed;
+    }
+
+
+
+    }
+
