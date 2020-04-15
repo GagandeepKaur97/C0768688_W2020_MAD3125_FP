a137941a4c9c9c26d12ca235774172c017c0223b
gagandeep kaur <sherry2312.gk@gmail.com>
2020-04-15 15:39:38 -0400

    [gk] validation mobile bill

--- app/src/main/java/com/gagan/c0768688_w2020_mad3125_fp/newmobileBill.java
+++ app/src/main/java/com/gagan/c0768688_w2020_mad3125_fp/newmobileBill.java
@@ -2,7 +2,9 @@ package com.gagan.c0768688_w2020_mad3125_fp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.google.android.material.textfield.TextInputEditText;
@@ -29,5 +31,36 @@ public class newmobileBill extends AppCompatActivity {
         MInternetUsage = findViewById(R.id.internetInternetUsaget);
         MinutesUsage = findViewById(R.id.mobileMinutesUsaget);
         MRegister = findViewById(R.id.mobilebutton);
+        MRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mID = MBillID.getText().toString().trim();
+                String mBillDate = MBillDate.getText().toString().trim();
+                String manufacturerName = ManufacturerName.getText().toString().trim();
+                String planName = PlanName.getText().toString().trim();
+                String mobileNumber = MobileNumber.getText().toString().trim();
+                String mInternetUsage = MInternetUsage.getText().toString().trim();
+                String minutesUsage = MinutesUsage.getText().toString().trim();
+
+                if(mID.isEmpty()){
+                    MBillID.setError("Please enter Bill ID");
+                } else if(mBillDate.isEmpty()){
+                    MBillDate.setError("Please enter Bill Date");
+                } else if(manufacturerName.isEmpty()){
+                    ManufacturerName.setError("Please enter Manufacturer Name");
+                } else if(planName.isEmpty()){
+                    PlanName.setError("Please enter Plan Name");
+                } else if(mobileNumber.isEmpty()){
+                    MobileNumber.setError("Please enter Mobile Number");
+                } else if(mInternetUsage.isEmpty()){
+                    MInternetUsage.setError("Please enter Internet Usage");
+                } else if(minutesUsage.isEmpty()){
+                    MinutesUsage.setError("Please enter Minutes Usage");
+                } else {
+                    Intent mint = new Intent(newmobileBill.this, Customer_screen.class);
+                    startActivity(mint);
+                }
+            }
+        });
     }
 }
