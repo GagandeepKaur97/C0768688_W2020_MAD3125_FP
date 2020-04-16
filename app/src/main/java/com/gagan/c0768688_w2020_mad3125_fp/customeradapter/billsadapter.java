4f4202e3b6f5195f3763ed536d0b1632503b5fe1
gagandeep kaur <sherry2312.gk@gmail.com>
2020-04-16 02:41:35 -0400

    [gk]bill adapter

--- app/src/main/java/com/gagan/c0768688_w2020_mad3125_fp/customeradapter/billsadapter.java
+++ app/src/main/java/com/gagan/c0768688_w2020_mad3125_fp/customeradapter/billsadapter.java
@@ -1,14 +1,41 @@
 package com.gagan.c0768688_w2020_mad3125_fp.customeradapter;
 
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.gagan.c0768688_w2020_mad3125_fp.R;
+import com.gagan.c0768688_w2020_mad3125_fp.classes.Bill;
 
-public class billsadapter {
+import java.util.ArrayList;
+
+public class billsadapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    public static ArrayList<Bill> billArrayList;
+
+    public billsadapter(ArrayList<Bill> billArrayList)
+    {
+        this.billArrayList = billArrayList;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
     public class billsViewHolder extends RecyclerView.ViewHolder {
 
         TextView txtCellBillId;
