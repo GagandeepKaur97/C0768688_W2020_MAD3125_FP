82c2996788119c82b5282bb39d666b72e8247eef
gagandeep kaur <sherry2312.gk@gmail.com>
2020-04-16 02:36:57 -0400

    [gk]bill adapter

--- app/src/main/java/com/gagan/c0768688_w2020_mad3125_fp/customeradapter/billsadapter.java
+++ app/src/main/java/com/gagan/c0768688_w2020_mad3125_fp/customeradapter/billsadapter.java
@@ -1,4 +1,25 @@
 package com.gagan.c0768688_w2020_mad3125_fp.customeradapter;
 
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.gagan.c0768688_w2020_mad3125_fp.R;
+
 public class billsadapter {
-}
+    public class billsViewHolder extends RecyclerView.ViewHolder {
+
+        TextView txtCellBillId;
+        TextView txtCellBillDate;
+        TextView txtCellBillType;
+        TextView txtCellBillAmount;
+        public billsViewHolder(@NonNull View itemView) {
+            super(itemView);
+            txtCellBillId = itemView.findViewById(R.id.txtCellBillId);
+            txtCellBillDate = itemView.findViewById(R.id.txtCellBillDate);
+            txtCellBillType = itemView.findViewById(R.id.txtCellBillType);
+            txtCellBillAmount = itemView.findViewById(R.id.txtCellBillAmount);
+
+}}}
