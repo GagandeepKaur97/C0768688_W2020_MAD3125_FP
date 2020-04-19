422d01074a9070e8fa2b3f2abb272fd5fcc5ce64
gagandeep kaur <sherry2312.gk@gmail.com>
2020-04-18 23:41:18 -0400

    [gk]StringExtension added

--- app/src/main/java/com/gagan/c0768688_w2020_mad3125_fp/Repo/StringExtension.java
+++ app/src/main/java/com/gagan/c0768688_w2020_mad3125_fp/Repo/StringExtension.java
@@ -1,4 +1,23 @@
 package com.gagan.c0768688_w2020_mad3125_fp.Repo;
 
 public class StringExtension {
+    public static boolean mobileValidation(String s)
+    {
+        if (s.matches("\\d{10}")) return true;
+        else return false;
+    }
+
+    public static boolean emailValidation(String s)
+    {
+        if (s.matches("[A-Z0-9a-z.%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}")) return true;
+        else return false;
+    }
+
+    public static String doubleFormatter(Double d)
+    {
+        return String.format("$ " + "%,.2f", d);
+    }
 }
+
+
+
